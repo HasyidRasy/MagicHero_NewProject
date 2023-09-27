b3ea452844932d3df6816c32e01618a10383346e
DimasAjisaka <81664698+DimasAjisaka@users.noreply.github.com>
2023-09-27 17:28:15 +0700

    Fix Camera Controller And Add Audio Manager
    Memperbaiki Camera Controller dan menambahkan Audio Manager, Demo_audioManager

--- Assets/Scripts/CharacterManagement/CameraController.cs
+++ Assets/Scripts/CharacterManagement/CameraController.cs
@@ -6,10 +6,6 @@ public class CameraController : MonoBehaviour {
     private Vector3 _offset;
     [SerializeField] private Transform target;
     [SerializeField] private float smoothTime;
-    [SerializeField] private float minX;
-    [SerializeField] private float maxX;
-    [SerializeField] private float minZ;
-    [SerializeField] private float maxZ;
     private Vector3 _currentVelocity = Vector3.zero;
 
     private void Awake() {
@@ -19,10 +15,6 @@ public class CameraController : MonoBehaviour {
     //Follow player logic
     private void LateUpdate() {
         Vector3 targetPosition = target.position + _offset;
-       
-        targetPosition.x = Mathf.Clamp(targetPosition.x, minX, maxX);
-        targetPosition.z = Mathf.Clamp(targetPosition.z, minZ, maxZ);
-
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothTime);
     }
 }
