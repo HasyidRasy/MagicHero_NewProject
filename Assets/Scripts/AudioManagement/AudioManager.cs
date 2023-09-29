b4ac1439a8762564605ddd06d5b2a9e2fef8c35a
DimasAjisaka <81664698+DimasAjisaka@users.noreply.github.com>
2023-09-29 17:01:42 +0700

    Add Audio Event
    Menambahkan audio event, beberapa logi play pada audio manager, dan ada perubahan pada camera controller untuk menerapkan event system audio

--- Assets/Scripts/AudioManagement/AudioManager.cs
+++ Assets/Scripts/AudioManagement/AudioManager.cs
@@ -3,5 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
+    //bgm
+    [SerializeField] private AudioSource bgmBattle;
+    [SerializeField] private AudioSource bgmMainMenu;
+    [SerializeField] private AudioSource bgmSafezone;
+    //sfx upgrade
+    [SerializeField] private AudioSource sfxUpgradeSuccess;
+    [SerializeField] private AudioSource sfxUpgradeScreenOpened;
+    [SerializeField] private AudioSource sfxUpgradeScreenClosed;
+    [SerializeField] private AudioSource sfxUpgradeScreenPopup;
+    //sfx battle
+    [SerializeField] private AudioSource sfxEnemyDestroy;
+    [SerializeField] private AudioSource sfxFireHit;
+    [SerializeField] private AudioSource sfxWaterHit;
+    [SerializeField] private AudioSource sfxAirHit;
+    //sfx player
+    [SerializeField] private AudioSource sfxAtk1;
+    [SerializeField] private AudioSource sfxAtk2;
+    [SerializeField] private AudioSource sfxAtk3;
+    [SerializeField] private AudioSource sfxAtk4;
+    [SerializeField] private AudioSource sfxDash;
+    [SerializeField] private AudioSource sfxDeath;
+    //sfx destructable object
+    [SerializeField] private GameObject sfxDestructStone;
+    [SerializeField] private GameObject sfxDestructWood;
+    //sfx Environment
+    [SerializeField] private AudioSource sfxStepOnGrass;
+    [SerializeField] private AudioSource sfxStepOnWater;
+    [SerializeField] private AudioSource sfxStepOnDirt;
+    [SerializeField] private AudioSource sfxDoorOpen;
+    [SerializeField] private AudioSource sfxDoorClose;
+    //sfx elemental reaction
+    [SerializeField] private AudioSource sfxSteam;
+    [SerializeField] private AudioSource sfxFreeze;
+    [SerializeField] private AudioSource sfxCombustion;
 
+    public void PlayBgmBattle() {
+        bgmBattle.Play();
+    }
+
+    public void PlayBgmMainMenu() {
+        bgmMainMenu.Play();
+    }
+
+    public void PlayBgmSafezone() {
+        bgmSafezone.Play();
+    }
+
+    public void PlaySfxDestructWood(Vector3 spawnPosition) {
+        GameObject.Instantiate(sfxDestructWood, spawnPosition, Quaternion.identity);
+    }
+
+    public void PlaySfxDestructStone(Vector3 spawnPosition) {
+        GameObject.Instantiate(sfxDestructStone, spawnPosition, Quaternion.identity);
+    }
+
+    public void PlaySfxDoorOpen() {
+        sfxDoorOpen.Play();
+    }
+
+    public void PlaySfxDoorClose() {
+        sfxDoorClose.Play();
+    }
 }
