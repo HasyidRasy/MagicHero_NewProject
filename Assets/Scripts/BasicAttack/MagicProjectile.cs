a76e9bb89b19fd68d8d348c205c95149110b6c19
RizkiRamadhan99 <rizkirmdn@gmail.com>
2023-10-05 11:04:13 +0700

    Update Basic Attack Mechanic
    Menambahkan mekanik elemental reaction, dan casting elemental magic projectile

--- Assets/Scripts/BasicAttack/MagicProjectile.cs
+++ Assets/Scripts/BasicAttack/MagicProjectile.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ElementalType
+{
+    Water,
+    Fire,
+    Wind
+}
 public class MagicProjectile : MonoBehaviour
 {
     [SerializeField] private ElementalType element; // Elemen proyektil
