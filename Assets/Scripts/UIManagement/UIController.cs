3a607ad258f7e4f52b391d866d4c8496d844ed5e
DimasAjisaka <81664698+DimasAjisaka@users.noreply.github.com>
2023-10-26 10:07:40 +0700

    Update Audio Manager
    Merubah beberapa setting audio

--- Assets/Scripts/UIManagement/UIController.cs
+++ Assets/Scripts/UIManagement/UIController.cs
@@ -9,6 +9,15 @@ public class UIController : MonoBehaviour {
     public Toggle _bgmToggle;
     public Toggle _sfxToggle;
 
+    private void Start() {
+        if (PlayerPrefs.HasKey("musicVolume")) {
+            LoadVolume();
+        } else {
+            BgmVolume();
+            SfxVolume();
+        }
+    }
+
     public void ToggleBgm() {
         NewAudioManager.Instance.ToggleBGM();
     }
@@ -23,5 +32,13 @@ public class UIController : MonoBehaviour {
 
     public void SfxVolume() {
         NewAudioManager.Instance.SfxVolume(_sfxSlider.value);
+    }
+
+    private void LoadVolume() {
+        _bgmSlider.value = PlayerPrefs.GetFloat("bgmVolume");
+        _sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+
+        BgmVolume();
+        SfxVolume();
     }
 }
