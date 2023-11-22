a30c318d24f2dbfadc34fdb7ac95294f68b2923a
Rafalff <rczfa27@gmail.com>
2023-11-22 22:18:20 +0700

    Add cooldown for basic attack & change sraff color accordingly
    Add cooldown and indicator what element is next
    Change staff color according what element is next

--- Assets/Scripts/CharacterManagement/NewPlayerController1.cs
+++ Assets/Scripts/CharacterManagement/NewPlayerController1.cs
@@ -11,6 +11,7 @@ public class NewPlayerController1 : MonoBehaviour
     private CharacterModel characterModel;
     private Animator animator;
     private ElementSwitchSystem elementSwitchSystem;
+    private CooldownAttackUI cooldownAtkUI;
     //cek dash logic
     private bool isDashing = false;
     //rigidbody
@@ -35,9 +36,9 @@ public class NewPlayerController1 : MonoBehaviour
     [SerializeField] private float magicProSpeed = 10f;         // Kecepatan proyektil
 
     [Header("Casting Speed")]
-    [SerializeField] private float timeBetweenAttacks = 0.5f;   // Waktu antara serangan
+    public float timeBetweenAttacks = 0.5f;   // Waktu antara serangan
     [SerializeField] private float timeBetweenSteps = 0.5f;   // Waktu antara serangan
-    private float attackCooldown = 0f;
+    public float attackCooldown = 0f;
     private float stepCooldown = 0f;
 
     [SerializeField] private LayerMask groundMask;
@@ -82,18 +83,22 @@ public class NewPlayerController1 : MonoBehaviour
         characterModel = GetComponent<CharacterModel>();
         animator = GetComponentInChildren<Animator>();
         elementSwitchSystem = FindObjectOfType<ElementSwitchSystem>();
+        cooldownAtkUI = FindObjectOfType<CooldownAttackUI>();
     }
 
     private void Start()
     {
         mainCamera = Camera.main;
         attackPattern[0] = elementalSlots[0];
+        cooldownAtkUI.SetElement(attackPattern[currentAttackIndex]);
     }
 
     private void Update()
     {
         PlayerStat();
-        attackCooldown -= Time.deltaTime;
+        if (attackCooldown > 0) {
+            attackCooldown -= Time.deltaTime;
+        }
         stepCooldown -= Time.deltaTime;
         _currentDashCd += Time.deltaTime;
 
@@ -291,8 +296,10 @@ public class NewPlayerController1 : MonoBehaviour
                 rb.velocity = targetDirection * magicProSpeed;
             }
             ChangeActiveElement();
-            // Menonaktifkan isShooting setelah menembak
-            StartCoroutine(DisableShootingForDuration(timeBetweenAttacks));
+            cooldownAtkUI.SetElement(elementalSlots[currentSlotIndex]);
+            Debug.Log(elementalSlots[currentSlotIndex]);
+        // Menonaktifkan isShooting setelah menembak
+        StartCoroutine(DisableShootingForDuration(timeBetweenAttacks));
     }
     public void ResetAttackIndex()
     {
