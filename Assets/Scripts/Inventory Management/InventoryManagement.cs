3c55c1991a9bbb65004441e3233a3168b5883360
RizkiRamadhan99 <rizkirmdn@gmail.com>
2023-11-30 15:56:12 +0700

    Add Border Rarity on Attribute

--- Assets/Scripts/Inventory Management/InventoryManagement.cs
+++ Assets/Scripts/Inventory Management/InventoryManagement.cs
@@ -6,19 +6,50 @@ using UnityEngine.UI;
 public class InventoryManagement : MonoBehaviour
 {
     public Transform buffIconContainer;
-    public GameObject buffIconPrefab;   
+    public Transform buffSlotsContainer;
+    public List<Image> buffSlots = new List<Image>();
+    public GameObject buffIconPrefab;
 
     public void UpdateBuffDisplay(List<UpgradeData> activeBuffs)
     {
+        // Clear the list before updating
+        buffSlots.Clear();
+
+        // Get all Image components from children of buffSlotsContainer
+        Image[] childImages = buffSlotsContainer.GetComponentsInChildren<Image>(true);
+        buffSlots.AddRange(childImages);
+
         foreach (Transform child in buffIconContainer)
         {
             Destroy(child.gameObject);
         }
 
-        foreach (UpgradeData buff in activeBuffs)
+        for (int i = 0; i < activeBuffs.Count; i++)
         {
+            UpgradeData buff = activeBuffs[i];
+
+            if (i < buffSlots.Count)
+            {
+                Image slot = buffSlots[i];
+
+                if (buff.rarity == UpgradeRarity.Common)
+                {
+                    slot.color = new Color(0.545f, 0.761f, 0.808f);
+                }
+                else if (buff.rarity == UpgradeRarity.Rare)
+                {
+                    slot.color = new Color(0.518f, 0.157f, 0.741f);
+                }
+                else
+                {
+                    slot.color = Color.white;
+                }
+            }
+
+            // Instantiate and set the buff icon
             GameObject buffIcon = Instantiate(buffIconPrefab, buffIconContainer);
-            buffIcon.GetComponent<Image>().sprite = buff.upgradeIcon;
+            Image iconImage = buffIcon.GetComponent<Image>();
+            iconImage.sprite = buff.upgradeIcon;
         }
     }
 }
