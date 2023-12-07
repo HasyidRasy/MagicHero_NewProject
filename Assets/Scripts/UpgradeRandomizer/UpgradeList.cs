f00fa1cf0ae126c1b18c514bee1c3b462519cbc9
AvinChailiSalim <86093166+AvinChailiSalim@users.noreply.github.com>
2023-12-07 09:50:21 +0700

    test change from tutorial

--- Assets/Scripts/UpgradeRandomizer/UpgradeList.cs
+++ Assets/Scripts/UpgradeRandomizer/UpgradeList.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UpgradeList : MonoBehaviour
 {
     public static UpgradeList Instance {get; private set;}
 
     public List<UpgradeData> chosenUpgrades = new List<UpgradeData>();
+
+    private string tutorialSceneName = "Tutorial";
 
     private void Awake()
     {
@@ -21,10 +24,27 @@ public class UpgradeList : MonoBehaviour
         }
     }
 
-
-    // Update is called once per frame
-    void Update()
+    void RemoveTutorialList()
     {
-        
+        chosenUpgrades.Clear(); // This line clears all elements from the list
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != tutorialSceneName && SceneManager.GetActiveScene().name == tutorialSceneName)
+        {
+            // The current scene is not the tutorial scene, but the previous one was
+            RemoveTutorialList();
+        }
     }
 }
